2512b2c4953aa97a77f76e284ac9be4f67263f41
yajunchu <337433736@qq.com>
2018-04-09 22:27:47 +0800

    /LOL

--- static/js/common.js
+++ static/js/common.js
@@ -14,7 +14,7 @@
 //     backToTopFun();
 // });
 
-const getRandomColor = function() {
+var getRandomColor = function() {
     var c = '#';
     var cArray = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
     for(var i = 0; i < 6;i++) {
@@ -23,3 +23,15 @@ const getRandomColor = function() {
     }
     return c;
 }
+var $backToTopEle = $('<a href="javascript:void(0)" class="Hui-iconfont toTop" title="返回顶部" alt="返回顶部" style="display:none">^</a>').appendTo($("body")).click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 120);
+});
+var backToTopFun = function() {
+    var st = $(document).scrollTop(),
+        winh = $(window).height();
+    (st > 0) ? $backToTopEle.show(): $backToTopEle.hide();
+    /*IE6下的定位*/
+    if(!window.XMLHttpRequest) {
+        $backToTopEle.css("top", st + winh - 166);
+    }
+};
