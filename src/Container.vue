5a714bc2bed22e247dd2e194bb7e2fd9b40f81ba
zhuyajun <zhuyajun2@maoyan.com>
2018-05-03 23:14:37 +0800

    z

--- src/Container.vue
+++ src/Container.vue
@@ -104,7 +104,43 @@
           //   // this.getArticleDetail(articleId);
           //   this.getLinks();
           // })
-          eventBus.$on('articleId',(articleId) => this.getArticleDetail(articleId))},
+          eventBus.$on('articleId',(articleId) => this.getArticleDetail(articleId))
+          (function (T, h, i, n, k, P, a, g, e) {
+            g = function () {
+              P = h.createElement(i);
+              a = h.getElementsByTagName(i)[0];
+              P.src = k;
+              P.charset = "utf-8";
+              P.async = 1;
+              a.parentNode.insertBefore(P, a)
+            };
+            T["ThinkPageWeatherWidgetObject"] = n;
+            T[n] || (T[n] = function () {
+              (T[n].q = T[n].q || []).push(arguments)
+            });
+            T[n].l = +new Date();
+            if (T.attachEvent) {
+              T.attachEvent("onload", g)
+            } else {
+              T.addEventListener("load", g, false)
+            }
+          }(window, document, "script", "tpwidget", "//widget.seniverse.com/widget/chameleon.js"));
+          tpwidget("init", {
+            "flavor": "slim",
+            "location": "WTW3SJ5ZBJUY",
+            "geolocation": "enabled",
+            "language": "zh-chs",
+            "unit": "c",
+            "theme": "chameleon",
+            "container": "tp-weather-widget",
+            "bubble": "enabled",
+            "alarmType": "badge",
+            "uid": "U350CADADE",
+            "hash": "3d1905d272c3bb08cc52c4c3e7ad4059"
+          });
+          tpwidget("show");
+          },
+
         methods:{
           getLinks: function () {
             this.$axios.get(this.apiLink).then((response) => {
