69ffa57d29ef7f239fb90821b08271995cf95e13
zhuyajun <zhuyajun@yupiao.com>
2018-05-04 17:47:54 +0800

    :recycle: set refere

--- src/Foot.vue
+++ src/Foot.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="w_foot">
-      <div class="w_foot_copyright">Copyright &copy; 2017-2020, www.strawman.xin. All Rights Reserved. <span>|</span>
+      <div class="w_foot_copyright"><span id="cnzz"></span>Copyright &copy; 2017-2020, www.strawman.xin. All Rights Reserved. <span>|</span>
             <a target="_blank" href="http://www.miitbeian.gov.cn/" rel="nofollow">浙ICP备16013152号-1</a>
       </div>
-      <div id="cnzz"></div>
     </div>
 
 </template>
