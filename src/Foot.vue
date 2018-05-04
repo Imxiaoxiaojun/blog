b0d7ea840c03e91496e7eef7d63481bbeb01fa4e
zhuyajun <zhuyajun@yupiao.com>
2018-05-04 18:55:47 +0800

    :recycle: set refere

--- src/Foot.vue
+++ src/Foot.vue
@@ -2,34 +2,32 @@
     <div class="w_foot">
       <div class="w_foot_copyright">Copyright &copy; 2017-2020, www.strawman.xin. All Rights Reserved. <span>|</span>
         <a target="_blank" href="http://www.miitbeian.gov.cn/" rel="nofollow">浙ICP备16013152号-1</a>
-        <span id="cnzz_stat_icon_1273601836"></span>
-        <remote-js src="http://s22.cnzz.com/z_stat.php?id=1273601836&show=pic"></remote-js>
+        <cnzz></cnzz>
       </div>
     </div>
 
 </template>
 
 <script>
-  export default {
-    data(){
-      cnzz_protocol: '';
-    },
+  var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+  var czSpan = {
+    template: unescape("%3Cspan id='cnzz_stat_icon_1273601836'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1273601836%26show%3Dpic' type='text/javascript'%3E%3C/script%3E")
+  }
+ export default {
     components: {
-      'remote-js': {
-        render(createElement) {
-          return createElement('script', { attrs: { type: 'text/javascript', src: this.src }});
-        },
-        props: {
-          src: { type: String, required: true },
-        },
-      },
+      czSpan
     },
-    mounted:function () {
-      // this.cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");
+    data()
+    {
 
-      /*const s = document.createElement('script');
-      s.type = 'text/javascript';
-      s.src = this.cnzz_protocol + 's22.cnzz.com/z_stat.php?id=1273601836&show=pic';*/
+    }
+  ,
+   mounted:function () {
+     // this.cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");
+
+     /*const s = document.createElement('script');
+     s.type = 'text/javascript';
+     s.src = this.cnzz_protocol + 's22.cnzz.com/z_stat.php?id=1273601836&show=pic';*/
       // document.body.appendChild(s);
 
      /* var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
