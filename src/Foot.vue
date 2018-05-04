d8c41a1e5a895f32b4d1d1ef6aa96ffb88cec65d
zhuyajun <zhuyajun@yupiao.com>
2018-05-04 16:44:19 +0800

    :recycle: set refere

--- src/Foot.vue
+++ src/Foot.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="w_foot">
-        <div class="w_foot_copyright">Copyright &copy; 2017-2020, www.strawman.xin. All Rights Reserved. <span>|</span>
+      <div class="w_foot_copyright">Copyright &copy; 2017-2020, www.strawman.xin. All Rights Reserved. <span>|</span>
             <a target="_blank" href="http://www.miitbeian.gov.cn/" rel="nofollow">浙ICP备16013152号-1</a>
-        </div>
+      </div>
+      <div id="cnzz"></div>
     </div>
+
 </template>
 
 <script>
   export default {
     mounted:function () {
       var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-      document.write(unescape("%3Cspan id='cnzz_stat_icon_1273601836'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1273601836%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
+      document.getElementById("cnzz").innerHTML += (unescape("%3Cspan id='cnzz_stat_icon_1273601836'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1273601836%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
+
       var _hmt = _hmt || [];
       (function() {
         var hm = document.createElement("script");
