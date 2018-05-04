2419b9e2c66f4bed0b2286d9290b7a8ad5e2fa6b
zhuyajun <zhuyajun@yupiao.com>
2018-05-04 18:05:47 +0800

    :recycle: set refere

--- src/Foot.vue
+++ src/Foot.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w_foot">
-      <div class="w_foot_copyright"><span id="cnzz"></span>Copyright &copy; 2017-2020, www.strawman.xin. All Rights Reserved. <span>|</span>
-            <a target="_blank" href="http://www.miitbeian.gov.cn/" rel="nofollow">浙ICP备16013152号-1</a>
+      <div class="w_foot_copyright">Copyright &copy; 2017-2020, www.strawman.xin. All Rights Reserved. <span>|</span>
+            <a target="_blank" href="http://www.miitbeian.gov.cn/" rel="nofollow">浙ICP备16013152号-1</a><span id="cnzz"></span>
       </div>
     </div>
 
@@ -11,15 +11,15 @@
   export default {
     mounted:function () {
       var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-      document.getElementById("cnzz").innerHTML += (unescape("%3Cspan id='cnzz_stat_icon_1273601836'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1273601836%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
+      document.getElementById("cnzz").innerHTML = (unescape("%3Cspan id='cnzz_stat_icon_1273601836'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1273601836%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
 
-      var _hmt = _hmt || [];
+      /*var _hmt = _hmt || [];
       (function() {
         var hm = document.createElement("script");
         hm.src = "https://hm.baidu.com/hm.js?82173aec295b8624fb31f98d97c6eb2a";
         var s = document.getElementsByTagName("script")[0];
         s.parentNode.insertBefore(hm, s);
-      })();
+      })();*/
     },
   }
 </script>
