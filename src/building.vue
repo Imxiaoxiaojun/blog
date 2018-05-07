9db23ac36a6a9b1953190c4f46c9c1b8c8b9d01f
zhuyajun <zhuyajun2@maoyan.com>
2018-05-07 09:12:10 +0800

    z

---
+++ src/building.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w_container">
+    <div class="container">
+      <div class="row w_main_row">
+
+        <ol class="breadcrumb w_breadcrumb">
+          <li><a href="">首页</a></li>
+          <li class="active"></li>
+          <span class="w_navbar_tip">像“草根”一样，紧贴着地面，低调的存在，冬去春来，枯荣无恙。</span>
+        </ol>
+
+        <div class="col-lg-9 col-md-9 w_main_left" style="height: 1000px;">
+          <div class="about">
+            <h2>建设中</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "IBuilding"
+  }
+</script>
+
+<style scoped>
+
+</style>
